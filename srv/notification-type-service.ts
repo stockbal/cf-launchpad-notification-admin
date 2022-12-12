a2594aa205860a16d232ae5c417820003f326226
stockbal <ludwig.stockbauer-muhr@msg.group>
2022-12-12 17:39:57 +0100

    chore: update prettier config

--- srv/notification-type-service.ts
+++ srv/notification-type-service.ts
@@ -2,7 +2,7 @@ import { ApplicationService } from "@sap/cds";
 
 export class NotificationTypeService extends ApplicationService {
   override async init() {
-    this.before("SAVE", async (req) => {
+    this.before("SAVE", async req => {
       //
     });
     await super.init();
